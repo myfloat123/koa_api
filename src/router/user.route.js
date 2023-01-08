d75af4ddbe92e04693b0e2860b5e750864ec76b7
xyf <2767252511@qq.com>
2023-01-09 00:57:59 +0800

    14-用户的认证

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router')
 
 const { userValidator, verifyUser, cryptPassword, verifyLogin } = require('../middleware/user.middleware')
+const { auth } = require('../middleware/auth.middleware')
+
 const { register, login } = require('../controller/user.controller')
 
 const router = new Router({ prefix: '/users' })
@@ -10,5 +12,11 @@ router.post('/register', userValidator, verifyUser, cryptPassword, register)
 
 // 登录接口
 router.post('/login', userValidator, verifyLogin, login)
+
+// 修改密码接口
+router.patch('/', auth, (ctx, next) => {
+  console.log(ctx.state.user)
+  ctx.body = '修改密码成功'
+})
 
 module.exports = router
